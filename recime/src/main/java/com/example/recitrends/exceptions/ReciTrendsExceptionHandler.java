2c72fac37e4403092532e4b5ea68d051775ebccd
Neil Diamzon <neildiamzon@Neils-MacBook-Air.local>
2024-08-30 22:21:23 +0800

    modify test cases

--- recime/src/main/java/com/example/recitrends/exceptions/ReciTrendsExceptionHandler.java
+++ recime/src/main/java/com/example/recitrends/exceptions/ReciTrendsExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.recitrends.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.UnsatisfiedServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -11,9 +12,10 @@ import com.example.recitrends.dto.ResponseMessage;
 @ControllerAdvice
 public class ReciTrendsExceptionHandler {
 	
-	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	@ExceptionHandler({MethodArgumentTypeMismatchException.class, 
+		UnsatisfiedServletRequestParameterException.class})
 	public ResponseEntity<ResponseMessage> handleMethodArguementTypeMismatchException(
-			MethodArgumentTypeMismatchException e){
+			Exception e){
 		String message = "A difficulty is required for filtering trending recipes.";
         return new ResponseEntity<>(new ResponseMessage(message), HttpStatus.BAD_REQUEST);
 	}
